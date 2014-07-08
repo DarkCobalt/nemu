5c2b292d59f2913ba888765823f9f9ed5df8c481
DarkCobalt <haker.inf@gmail.com>
2014-07-08 15:26:14 +0000

    upstream

--- nemu/nemu/urls.py
+++ nemu/nemu/urls.py
@@ -12,12 +12,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('api.urls'), name='api'),
     url(r'^$', views.OnePageAppView.as_view(), name='home'),
-    (r'^rest-auth/', include('rest_auth.urls')),
+    url(r'^rest-auth/', include('rest_auth.urls')),
 
-)
-
-
-IGNORABLE_404_URLS = (
-    re.compile(r'\.(php|cgi)$'),
-    re.compile(r'^/phpmyadmin/'),
 )
